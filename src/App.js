7d292841e609b2c3a1944e2b9c7e41e409b28d8a
Krastii <66555471+Krastii@users.noreply.github.com>
2021-07-27 12:33:06 +0300

    add comment and removed unused code

--- src/App.js
+++ src/App.js
@@ -4,46 +4,37 @@ import React,{useReducer,useRef} from "react";
 import { Stage, Sprite, Container, useTick} from '@inlet/react-pixi';
 
 
-// function importAll(r) {
-//   let images = {};
-//   r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-//   return images;
-// }
-
-// const images = importAll(require.context('./image', false, /\.(png|jpe?g|svg)$/));
-
-
 
 let timer = 0,y=[];
 
-for(let i = 0;i<10;i++){
+for(let i = 0;i<10;i++){//задаем количество изображений в пуле
   y.push(i)
 }
 
-console.log(y);
 
 const changeState =()=>{
   if(timer === 0 ){
-    timer = 1000
-    setTimeout(()=>{timer = 0},2000)
+    timer = 1000//любое не нулевое значение
+    setTimeout(()=>{timer = 0},1000)//время кручения барабана
+    //можно добавить случайность сделав время рандомом в определённом диапазоне
   }
 
 }
 
 
-
 const reducer = (_, { data }) => data
+
 const Image =(props)=>{
    const [motion, update] = useReducer(reducer)
-    const iter = useRef(props.y*200)
+    const iter = useRef(props.y*200)//указание начальной точки каждого изображения
     useTick(delta => {
       
-      if(iter.current >= 2000){
-        iter.current=0
+      if(iter.current >= 2000){//возврат картинки наверх
+        iter.current=iter.current-2000
       }
       let i= iter.current
       if(timer>0){
-       i = (iter.current += 50)
+       i = (iter.current += 50)//скорость прокрутки
       
       }
        update({
@@ -56,9 +47,9 @@ const Image =(props)=>{
     })
   return(
     <Sprite 
-    image ={pirat}
+    image ={pirat}//в зависимости от y можно использовать разные изображения 
     anchor={0.5}
-    scale={{ x: 1, y: 1 }}
+    scale={{ x: 1, y: 1 }}//редактировать размер изображения
      {...motion}
     />
   )
